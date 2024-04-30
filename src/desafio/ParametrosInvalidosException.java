b489d6bdff7f1474469096142777b3c72bce59fe
Jhonanthan Campos <dionathancampos@hotmail.com>
2024-04-30 17:27:43 -0300

    commit 2

---
+++ src/desafio/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+package desafio;
+
+public class ParametrosInvalidosException extends Throwable {
+   
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String msg){
+        super(msg);
+    }
+	
+}
